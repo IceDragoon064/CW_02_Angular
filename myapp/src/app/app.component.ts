714f68dbea54b2fee953a72332c581e29646b4db
Leon Lu <ltlu1993@gmail.com>
2022-06-06 14:45:18 -0400

    Enabling route parameters

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   template: `<h1>This is the header</h1>
   <nav>
   <a routerLink="/page-a" routerLinkActive="active">A</a> |
-  <a routerLink="/page-b" routerLinkActive="active">B</a>
+  <a routerLink="/page-b/5/bob" routerLinkActive="active">B</a>
   </nav>
   <!-- Where router should display a view -->
   <router-outlet></router-outlet>`,
