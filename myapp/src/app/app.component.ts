bbd32674f4568df36fdce75c5358772e7bed5900
Leon Lu <ltlu1993@gmail.com>
2022-06-05 21:58:55 -0400

    pipe exercise

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -3,18 +3,28 @@ import { ConversionCtoF } from './ConversionCtoF';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello world! {{title}}</h1><ul>{{f}}</ul>`,
+  template: `<!-- Sep 17, 2019 -->
+{{ mydate | date }}<br>
+  <!-- Sep 17, 2019 -->
+  <p>{{ mydate | date: 'y MMMM d' }}<br>
+  <!-- Sep 17, 2019 -->
+  <p>{{ mydate | date: 'mediumDate' }}<br>
+  <!-- Tuesday, Sep 17, 2019 -->
+  <p>{{ mydate | date: 'fullDate' }}<br>
+  <!-- 6:00 PM -->
+  <p>{{ mydate | date: 'shortTime' }}<br>
+  <!-- TUESDAY, Sep 17, 2019 -->
+  <p>{{ mydate | date:'fullDate' | uppercase}}</p>
+  <!-- Sep 17, 2019 6:00PM -->
+  <p>{{ mydate | date:'MMMM d, y h:mma' }}</p>
+  `,
   styleUrls: ['./app.component.css'],
   providers: [ConversionCtoF]
 })
 export class AppComponent {
   title = 'This is Angular!';
 
-  f: number;
-  // Create instance of 'MyDataService' right in the constructor
-  // header.
-  constructor(conversionCtoF: ConversionCtoF) {
-  // Use service to call getNames() method.
-  this.f = conversionCtoF.getConversion();
-  }
+// Months start counting at 0.
+mydate = new Date(2019, 8, 17, 18, 0, 30);
+
 }
