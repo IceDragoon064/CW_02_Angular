5d38734695981a3be1182f42a40c7604244fc570
Leon Lu <ltlu1993@gmail.com>
2022-06-05 21:39:39 -0400

    Service exercise

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MyDataService } from './myDataService';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello world! {{title}}</h1> <app-second [firstName] = "author"></app-second>',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Hello world! {{title}}</h1><ul><li *ngFor="let name of names">{{name}}</li></ul>',
+  styleUrls: ['./app.component.css'],
+  providers: [MyDataService]
 })
 export class AppComponent {
   title = 'This is Angular!';
-  public author = "Leon";
+  names: Array<any>;
+
+  // Create instance of 'MyDataService' right in the constructor
+  // header.
+  constructor(myDataService: MyDataService) {
+     // Use service to call getNames() method.
+    this.names = myDataService.getNames();
+  }
+
+
 }
