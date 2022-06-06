b4c1589270e6ee1823647fc8bb035759e668e239
Leon Lu <ltlu1993@gmail.com>
2022-06-05 22:03:21 -0400

    Exercise 9

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -3,20 +3,10 @@ import { ConversionCtoF } from './ConversionCtoF';
 
 @Component({
   selector: 'app-root',
-  template: `<!-- Sep 17, 2019 -->
+  template: `<!-- Sep 17, 2019 6:00 PM -->
 {{ mydate | date }}<br>
   <!-- Sep 17, 2019 -->
-  <p>{{ mydate | date: 'y MMMM d' }}<br>
-  <!-- Sep 17, 2019 -->
-  <p>{{ mydate | date: 'mediumDate' }}<br>
-  <!-- Tuesday, Sep 17, 2019 -->
-  <p>{{ mydate | date: 'fullDate' }}<br>
-  <!-- 6:00 PM -->
-  <p>{{ mydate | date: 'shortTime' }}<br>
-  <!-- TUESDAY, Sep 17, 2019 -->
-  <p>{{ mydate | date:'fullDate' | uppercase}}</p>
-  <!-- Sep 17, 2019 6:00PM -->
-  <p>{{ mydate | date:'MMMM d, y h:mma' }}</p>
+  <p>{{ mydate | date: 'EEEE, MMMM d, y, h:mma' }}</p>
   `,
   styleUrls: ['./app.component.css'],
   providers: [ConversionCtoF]
